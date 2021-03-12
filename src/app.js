48ccf63b7a22e0b89b90a1372ad0a841f4db0545
Aditi Khandelwal <aditikhandelwal2000@gmail.com>
2021-03-13 00:07:10 +0530

    content added

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+require('./db/mongoose')
+const User = require('./models/course')
+// to put routes in separate files 
+const courseRouter = require('./routers/course')
+ 
+
+const app =  express()
+
+app.use(express.json())
+app.use(courseRouter)
+
+
+module.exports = app
